e36a6b2e6fffdc1614243afd8532e585e685e50a
alice-pt <alice_v90@yahoo.com>
2024-10-03 16:21:24 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,9 @@
+const images = document.querySelectorAll("#image");
+const describes = document.querySelectorAll("#describes");
+
+images.forEach(image => {
+
+    document.addEventListener("mouseover", () => {
+       .classList.remove("hidde")
+    })
+})
